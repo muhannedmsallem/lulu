ae9fd107ffd00bd4ad31a1da435415c99513526b
muhannedmsallem <muhannedmsallem@gmail.com>
2024-06-06 12:33:53 +0300

    niopjt

--- app/sw.js
+++ app/sw.js
@@ -1,22 +1,11 @@
-"use client";import { useEffect } from "react";
+"use client";
+import { useEffect } from "react";
 
 export default function Sw() {
   useEffect(() => {
     console.log('useEffect called');
     if (typeof window !== "undefined") {
       console.log('Window is defined');
-      
-      // Initialize OneSignal
-      window.OneSignal = window.OneSignal || [];
-      window.OneSignal.push(function() {
-        window.OneSignal.init({
-          appId: '4ff98f18-90a0-42c2-93e2-dc531efff17e',
-          allowLocalhostAsSecureOrigin: true,
-          notifyButton: {
-            enable: true,
-          },
-        });
-      });
 
       // Register service worker
       if ('serviceWorker' in navigator) {
@@ -24,7 +13,20 @@ export default function Sw() {
         navigator.serviceWorker.register('/service-worker.js')
           .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
-            // OneSignal SDK must be initialized after service worker registration
+            
+            // Initialize OneSignal
+            window.OneSignal = window.OneSignal || [];
+            window.OneSignal.push(function() {
+              window.OneSignal.init({
+                appId: '4ff98f18-90a0-42c2-93e2-dc531efff17e',
+                allowLocalhostAsSecureOrigin: true,
+                notifyButton: {
+                  enable: true,
+                },
+              });
+            });
+
+            // Register for push notifications after service worker registration
             window.OneSignal.registerForPushNotifications();
           })
           .catch(error => {
