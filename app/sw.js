9a283a8c40cd57990aec88eab046affe4edc2cee
muhannedmsallem <muhannedmsallem@gmail.com>
2024-06-06 12:06:48 +0300

    fdocdk

--- app/sw.js
+++ app/sw.js
@@ -6,11 +6,27 @@ export default function Sw() {
     console.log('useEffect called');
     if (typeof window !== "undefined") {
       console.log('Window is defined');
+      
+      // Initialize OneSignal
+      window.OneSignal = window.OneSignal || [];
+      window.OneSignal.push(function() {
+        window.OneSignal.init({
+          appId: 'Y4ff98f18-90a0-42c2-93e2-dc531efff17e',
+          allowLocalhostAsSecureOrigin: true,
+          notifyButton: {
+            enable: true,
+          },
+        });
+      });
+
+      // Register service worker
       if ('serviceWorker' in navigator) {
         console.log('Service Worker is supported');
         navigator.serviceWorker.register('/service-worker.js')
           .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
+            // OneSignal SDK must be initialized after service worker registration
+            window.OneSignal.registerForPushNotifications();
           })
           .catch(error => {
             console.error('Service Worker registration failed:', error);
